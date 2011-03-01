2417d0cb96d6ce282c178196d59137ff43987da3
andre <andre@andre-VirtualBox.(none)>
2011-03-01 16:44:18 +0100

    fix in config

--- _config.php
+++ _config.php
@@ -8,10 +8,6 @@ DataObject::add_extension('Member', 'FacebookMember');
 // For custom add and update methods extend FacebookMember and add your Extended FacebookMember to Member instead
 // DataObject::add_extension('Member', 'ExtendedFacebookMember');
 
-
-// adds the default updateFacebookFields method to member, alter your own decorator here for special update behaviour
-DataObject::add_extension('Member', 'UpdateFacebookMemberDefault');
-
 // adds the authenticator to the built in login form
 Authenticator::register('FacebookAuthenticator');
 

880bfa2f442b62a3727a9be6745a8aeda10e5be0
Will Rossiter <will.rossiter@gmail.com>
2011-03-02 19:52:34 +1300

    BUGFIX: added missing controller extension

--- _config.php
+++ _config.php
@@ -5,10 +5,11 @@
 
 // adds an extension hook to member
 DataObject::add_extension('Member', 'FacebookMember');
-// For custom add and update methods extend FacebookMember and add your Extended FacebookMember to Member instead
-// DataObject::add_extension('Member', 'ExtendedFacebookMember');
 
 // adds the authenticator to the built in login form
 Authenticator::register('FacebookAuthenticator');
+
+// add the facebook controller
+DataObject::add_extension('Controller', 'FacebookConnect');
 
 // don't forget to add Facebook App Keys to mysite/_config.php

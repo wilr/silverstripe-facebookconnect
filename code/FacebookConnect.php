8b66dc226bca9a18c88f58465b0c1e087c8be046
Will Rossiter <will.rossiter@gmail.com>
2012-04-29 15:22:25 +1200

    MINOR: trigger reload when logging out.

--- code/FacebookConnect.php
+++ code/FacebookConnect.php
@@ -315,7 +315,10 @@ window.fbAsyncInit = function() {
 		xfbml   : true
     });
 
-	FB.Event.subscribe('auth.login', function() {
+	FB.Event.subscribe('auth.login', function(response) {
+		window.location.reload();
+	});
+	FB.Event.subscribe('auth.logout', function(response) {
 		window.location.reload();
 	});
 };

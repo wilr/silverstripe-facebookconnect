4e014ca74b59a1adab54809b0a56f6308b33df2b
Will Rossiter <will@fullscreen.io>
2019-01-17 12:22:47 +1300

    Removed permissions getter for config

--- code/forms/FacebookLoginForm.php
+++ code/forms/FacebookLoginForm.php
@@ -7,31 +7,35 @@
  */
 class FacebookLoginForm extends MemberLoginForm
 {
-    
+
     protected $authenticator_class = 'FacebookAuthenticator';
-    
+
     public function __construct($controller, $name, $fields = null, $actions = null, $checkCurrentUser = true)
     {
         if ($checkCurrentUser && Member::currentUser() && Member::logged_in_session_exists()) {
             $fields = new FieldList(
                 new HiddenField("AuthenticationMethod", null, $this->authenticator_class, $this)
             );
-            
+
             $actions = new FieldList(
                 new FormAction("logout", _t('Member.BUTTONLOGINOTHER', "Log in as someone else"))
             );
         } else {
-            $fields = new FieldList(
-                new LiteralField('FacebookLoginIn', "<fb:login-button scope='". $controller->getFacebookPermissions() ."'></fb:login-button>")
+            $permissions = Config::inst()->get(
+                'FacebookControllerExtension', 'permissions'
             );
-            
+
+            $fields = new FieldList(
+                new LiteralField('FacebookLoginIn', "<fb:login-button scope='". implode(',', $permissions) ."'></fb:login-button>")
+            );
+
             $actions = new FieldList(
                 new LiteralField('FacebookLoginLink', "<!-- <a href='".$controller->getFacebookLoginLink() ."'>". _t('FacebookLoginForm.LOGIN', 'Login') ."</a> -->")
             );
         }
-        
+
         $backURL = (isset($_REQUEST['BackURL'])) ? $_REQUEST['BackURL'] : Session::get('BackURL');
-        
+
         if (isset($backURL)) {
             $fields->push(new HiddenField('BackURL', 'BackURL', $backURL));
         }
